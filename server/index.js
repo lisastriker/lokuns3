6a73c046ceb2dd7a2ca4996c857fd2b055498226
Melissa <lisastriker@gmail.com>
2021-08-23 22:44:29 +0800

    Set proxy

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const accountSid = process.env.REACT_APP_TWILIO_ACCOUNT_SID
 const authToken = process.env.REACT_APP_TWILIO_AUTH_TOKEN
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 const client = require('twilio')(accountSid, authToken);
   const app = express();
